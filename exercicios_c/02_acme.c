cd71faa1154860a7c4976073883d1f35b6b9f01a
Gustavo Concordia <167263812+Co5c0rdi4@users.noreply.github.com>
2025-08-15 11:49:50 -0300

    Update 02_acme.c

--- exercicios_c/02_acme.c
+++ exercicios_c/02_acme.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-
+//Desenha na tela a logomarca da empresa ACME em ASCII art.
 int main() {
     printf("  __ _  ___ _ __ ___   ___  \n");
     printf(" / _` |/ __| '_ ` _ \\ / _ \\ \n");
     printf("| (_| | (__| | | | | |  __/ \n");
     printf(" \\__,_|\\___|_| |_| |_|\\___|  \n");
 }
+
